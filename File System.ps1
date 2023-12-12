56c65c47f88a192fc6c8e15b82cbf7ff35fdfce5
Auke Bruinsma <github@air2.dds.nl>
2023-12-12 12:52:03 +0100

    Added more tooltips to gui elements

--- File System.ps1
+++ File System.ps1
@@ -30,13 +30,15 @@ function Idm-SystemInfo {
             @{
                 name = 'paths_spec'
                 type = 'textbox'
-                label = 'Paths'
+                label = 'P
+		tooltip = "Paths to collect. Separate multiple paths by '|'. Optionally suffix path with ':<n>' to collect 'n' levels deep."
                 value = ''
             }
             @{
                 name = 'excludes'
                 type = 'textbox'
                 label = 'Excludes'
+		tooltip = "File name patterns to exclude. Separate multiple patterns by '|'. E.g. *\example excludes all folders with the name 'example' and their contents; *\example\* excludes the contents of all folders with the name 'example', not the folder itself."
                 value = ''
             }
              @{
@@ -49,9 +51,9 @@ function Idm-SystemInfo {
                 name = 'recursion_depth'
                 type = 'textbox'
                 label = 'Recursion Depth'
-                description = 'Max. depth of recursion'
+                tooltip = 'Max. depth of recursion'
                 value = 1
-				hidden = '!recursive'
+		hidden = '!recursive'
             }
 			
             @{
